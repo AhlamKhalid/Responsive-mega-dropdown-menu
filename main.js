9867f87736299727f7368a57db4b5a5931692b26
AhlamKhalid <ahlamkarkashan@gmail.com>
2022-10-04 21:53:24 +0300

    show/hide navigation menu

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 // elements
 const menuButton = document.getElementById("menu-button");
 const closeButton = document.getElementById("close-button");
+const navigationMenu = document.getElementById("navigation-menu");
 
 menuButton.addEventListener("click", () => {
   menuButton.classList.add("hide");
   closeButton.classList.add("show");
+  navigationMenu.classList.add("show");
 });
 
 closeButton.addEventListener("click", () => {
   menuButton.classList.remove("hide");
   closeButton.classList.remove("show");
+  navigationMenu.classList.remove("show");
 });
